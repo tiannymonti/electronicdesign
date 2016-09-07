62f4194c0dab64fb919cb6b7ddfb6d0b55621293
tiannymonti <tiannymonti@gmail.com>
2016-09-07 02:45:31 -0500

    7

--- hist.php
+++ hist.php
@@ -14,10 +14,6 @@
 	  <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 	  
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <!--Para que el tiempo se vea decente-->
-<!--
-      <link rel="stylesheet" href="pickadate.js/lib/themes/classic.css">
--->
     </head>
 
     <body>
