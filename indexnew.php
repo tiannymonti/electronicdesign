83c4fc9b0ad5520515596485ec5b947e508537e8
tiannymonti <tiannymonti@gmail.com>
2016-09-09 17:37:52 -0500

    erda

--- indexnew.php
+++ indexnew.php
@@ -34,7 +34,7 @@
 		<div class="row">
 		<div id="googleMap" style="width:95%;height:50em;margin:auto; margin-top:0.5em;"></div>
 		<div class="divider"></div>
-		<div class="col s4 push-s4" id="division"><span class="flow-text">Tiempo: </span><span class="flow-text" id="time">0000-00-00 00:00:00</span></div>
+		<div class="col s4" id="division"><span class="flow-text">Tiempo: </span><span class="flow-text" id="time">0000-00-00 00:00:00</span></div>
 		<div class="col s4 push-s4" id="division"><span class="flow-text">Latitud: </span><span class="flow-text" id="latitud">00000000</span></div>
 		<div class="col s4 pull-s4" id="division"><span class="flow-text">Longitud: </span><span class="flow-text" id="longitud">00000000</span></div>
 		</div>
