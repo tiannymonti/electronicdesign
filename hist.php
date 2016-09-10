978a26787fd521f19eab9502a705f7fb8f851fe4
tiannymonti <tiannymonti@gmail.com>
2016-09-10 17:13:49 -0500

    prueba ajax 1

--- hist.php
+++ hist.php
@@ -177,25 +177,32 @@
 								myPositions.push(myCenter);
 							}
 						}
-						function initMap() {
-							map = new google.maps.Map(document.getElementById("googleMap"), {
-							zoom: 3,
-							center: myCenter,
-							mapTypeId: google.maps.MapTypeId.ROADMAP
-							});
-						  var myPath = new google.maps.Polyline({
-							path: myPositions,
-							geodesic: true,
-							strokeColor: '#FF0000',
-							strokeOpacity: 1.0,
-							strokeWeight: 2
-						  });
-						
-							myPath.setMap(map);
-						}
+						alert(myPositions);						
+					}  //fin de la funcion de response					
+				});          
+      	
+      	alert(myPositions);
+		 function initMap() {
+			map = new google.maps.Map(document.getElementById('googleMap'), {
+			zoom: 3,
+			center: myCenter,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+
+	  
+	  var myPath = new google.maps.Polyline({
+		path: myPositions,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	  });
+
+	  myPath.setMap(map);
+	}
 	
-						};						
-					});          		 
+	  };	
+
    </script>
 
 <!--
