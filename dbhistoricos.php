57d5cd0b6d585853d420757cbdeb668f6d1c091e
tiannymonti <tiannymonti@gmail.com>
2016-09-10 10:50:52 -0500

    muestra errores

--- dbhistoricos.php
+++ dbhistoricos.php
@@ -6,31 +6,35 @@
 	$fecha2 = $_POST['fecha2'];
 	$hora2 = $_POST['hora2'];
 	
-	$desde = $fecha1 . " " . $hora1);
-	$hasta = $fecha2 . " " . $hora2);
+	$desde = htmlspecialchars($fecha1 . " " . $hora1);
+	$hasta = htmlspecialchars($fecha2 . " " . $hora2);
 	
 	//echo $desde.' - '.$hasta;
 
     //// Create connection
-    $tion = mysqli_connect("localhost", "root", "1234", "coordenadas");
+    //$tion = mysqli_connect("localhost", "root", "1234", "coordenadas");
+    
+    $query = "SELECT latitud, longitud FROM cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.';";
+    echo $query;
+    
 
         // Consulta de selección 
-    $querytime = mysqli_query($tion, "SELECT latitud, longitud FROM cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.' ORDER BY time;");                             
-    if (!$querytime) {
-		die('Consulta no válida: ' . mysql_error());
-	}
-    //// set array
-	//$positions = array();
+    //$querytime = mysqli_query($tion, "SELECT latitud, longitud FROM cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.';");                             
+    //if (!$querytime) {
+		//die('Consulta no válida: ' . mysql_error());
+	//}
+    ////// set array
+	////$positions = array();
 	
-	// look through query
-	while($row = mysqli_fetch_assoc($querytime)){
-		echo $row['latitud'];
-		echo $row['longitud'];
-	}
+	//// look through query
+	//while($row = mysqli_fetch_assoc($querytime)){
+		//echo $row['latitud'];
+		//echo $row['longitud'];
+	//}
 	
-	//$location = array();
-	//$location = array_map('current', $positions);  
+	////$location = array();
+	////$location = array_map('current', $positions);  
 	
-	//echo json_encode($location);              
+	////echo json_encode($location);              
               
 ?>
