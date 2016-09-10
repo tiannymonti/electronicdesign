b30d858198776bb48d8e94d61de45a8440b7bbbf
tiannymonti <tiannymonti@gmail.com>
2016-09-10 12:25:57 -0500

    prueba ajax 1

--- hist.php
+++ hist.php
@@ -56,7 +56,7 @@
 		</div>
 		<div class="row">
 		<!--<div id="googleMap" style="width:95%;height:50em;margin:auto; margin-top:0.5em;"></div>-->
-		<div class="col s12" id="division"><h1> HOLI </h1></div>
+		<div class="col s12"><h1 id="division"> HOLI </h1></div>
 		</div>
 	  </main>		  
       <footer name="abajo" class="page-footer light-blue darken-4">
@@ -163,8 +163,7 @@
                 success:  function (response) {
                         $("#division").html(response);
                 }
-			});
-            //post("dbhistoricos.php", {fecha1:fecha1, hora1:hora1, fecha2:fecha2, hora2:hora2})     
+			});  
         };
         
         
