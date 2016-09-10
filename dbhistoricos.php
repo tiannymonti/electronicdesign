632c2b56fe9a79484f61356a6ad15067a34fa39e
tiannymonti <tiannymonti@gmail.com>
2016-09-10 10:02:27 -0500

    debuggin

--- dbhistoricos.php
+++ dbhistoricos.php
@@ -6,33 +6,29 @@
 	$fecha2 = $_POST['fecha2'];
 	$hora2 = $_POST['hora2'];
 	
-	$desde = $fecha1 . " " . $hora1;
-	$hasta = $fecha2 . " " . $hora2;
+	$desde = htmlspecialchars($fecha1 . " " . $hora1);
+	$hasta = htmlspecialchars($fecha2 . " " . $hora2);
 	
 	//echo $desde.' - '.$hasta;
 
-    // Create connection
-    $tion = mysqli_connect("localhost", "root", "1234", "coordenadas");
-    // Check connection
-    if ($tion->connect_error) {
-        die("Connection failed: " . $tion->connect_error);
-    } 
+    //// Create connection
+    //$tion = mysqli_connect("localhost", "root", "1234", "coordenadas");
 
-        // Consulta de selección 
-    $querytime = mysqli_query($tion, "SELECT latitud, longitud FROM coordenadas.cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.' ORDER BY time";);                             
+        //// Consulta de selección 
+    //$querytime = mysqli_query($tion, "SELECT latitud, longitud FROM coordenadas.cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.' ORDER BY time;");                             
     
-    echo $querytime;
+    //echo $querytime;
     
-    // set array
-	$positions = array();
-	// look through query
-	while($row = mysqli_fetch_assoc($querytime)){
-		$positions[] = $row;
-	}
+    //// set array
+	//$positions = array();
+	//// look through query
+	//while($row = mysqli_fetch_assoc($querytime)){
+		//$positions[] = $row;
+	//}
 	
-	$location = array();
-	$location = array_map('current', $positions);  
+	//$location = array();
+	//$location = array_map('current', $positions);  
 	
-	echo json_encode($location);              
+	//echo json_encode($location);              
               
 ?>
