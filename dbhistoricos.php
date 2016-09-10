f3514af2bf72d9ea2ca625ad52503c29555aa8d0
tiannymonti <tiannymonti@gmail.com>
2016-09-10 10:45:13 -0500

    muestra errores

--- dbhistoricos.php
+++ dbhistoricos.php
@@ -15,7 +15,7 @@
     $tion = mysqli_connect("localhost", "root", "1234", "coordenadas");
 
         // Consulta de selección 
-    $querytime = mysqli_query($tion, "SELECT latitud, longitud FROM cordenadas WHERE time BETWEEN '.$desde.' AND '.$hasta.' ORDER BY time;");                             
+    $querytime = mysqli_query($tion, "SELECT latitud, longitud FROM cordenadas WHERE time BETWEEN '$desde' AND '$hasta' ORDER BY time;");                             
     if (!$querytime) {
 		die('Consulta no válida: ' . mysql_error());
 	}
