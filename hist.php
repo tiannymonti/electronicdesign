7dd0136a3c198cd961d19cd71dc9f0c74c14cfb0
tiannymonti <tiannymonti@gmail.com>
2016-09-10 22:50:46 -0500

    validacion

--- hist.php
+++ hist.php
@@ -113,8 +113,15 @@
 			}			
 		});
 		var picker1 = $input1.pickadate('picker');
-		
-		var $tinput1 = $('.start-timepicker').pickatime({
+	    if ( picker1.get('value') ) {
+			picker2.set('min', picker1.get('select'))
+		 };
+		picker1.on('set', function(event) {
+			if ( event.select ) {
+				picker2.set('min', picker1.get('select'))
+			}
+		});           
+  		var $tinput1 = $('.start-timepicker').pickatime({
 			clear: 'Borrar',
 			close: 'Cerrar',
 			onClose: function(){
@@ -140,7 +147,15 @@
 			}			
 		});
 		var picker2 = $input2.pickadate('picker');
-		
+	   if ( picker2.get('value') ) {
+			picker1.set('max', picker2.get('select'))
+		};
+		picker2.on('set', function(event) {
+		if ( event.select ) {
+			picker1.set('max', picker2.get('select'))
+		}
+		});
+  	
 		var $tinput2 = $('.end-timepicker').pickatime({
 			clear: 'Borrar',
 			close: 'Cerrar',
@@ -185,29 +200,7 @@
         pickert1.open();
         picker2.open();
         pickert2.open();
-        //var fecha1 = picker1.get('select', 'yyyy-mm-dd');
-          if ( picker1.get('value') ) {
-			picker2.set('min', picker1.get('select'))
-		 };
-		picker1.on('set', function(event) {
-			if ( event.select ) {
-				picker2.set('min', picker1.get('select'))
-			}
-			});           
-       //var hora1 = pickert1.get('select', 'HH:i');
-       //hora1 = hora1.concat(":00");
-       //var fecha2 = picker2.get('select', 'yyyy-mm-dd');
-       if ( picker2.get('value') ) {
-			picker1.set('max', picker2.get('select'))
-		};
-		picker2.on('set', function(event) {
-		if ( event.select ) {
-			picker1.set('max', picker2.get('select'))
-		}
-		});
-         //var hora2 = pickert2.get('select', 'HH:i');
-          //  hora2 = hora2.concat(":00"); 
-		
+    
 		 function toggleFunction() {
             //picker1.open();
             //pickert1.open();
