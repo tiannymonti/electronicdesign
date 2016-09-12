4e78f76b04cf16060f7a1925e27e7954fde64b14
tiannymonti <tiannymonti@gmail.com>
2016-09-11 22:25:13 -0500

    markers

--- hist.php
+++ hist.php
@@ -55,7 +55,7 @@
 		</div>
 		</div>
 		<div class="row">
-		  <div class="preloader-wrapper big active" style="display:none; text-align: center;" id="preloader">
+		  <div class="preloader-wrapper big active" style="display:none; margin:auto;" id="preloader">
 			<div class="spinner-layer spinner-blue-only">
 			  <div class="circle-clipper left">
 				<div class="circle"></div>
