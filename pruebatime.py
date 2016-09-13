6e41e5c7f9f116cc90bda692b4d92626cdf83bbd
tiannymonti <tiannymonti@gmail.com>
2016-09-13 12:58:09 -0500

    Hecho por R

--- pruebatime.py
+++ pruebatime.py
@@ -1,6 +1,24 @@
 import mysql.connector
 import mysql
-time = '2016-09-13 10:59:29'
+
+hora = '10'
+minu= '30'
+seg = '11'
+dia1 = '13'
+mes = '9'
+year = '2016'
+
+if (hora >= 1) & (hora <= 5):
+    hora = int(hora)
+    hora = 19 + hora
+    dia1 = dia1 - 1
+    dia1 = str(dia1)
+else:
+    hora = int(hora)
+    hora = hora - 5
+    hora = str(hora)
+                       
+time = year+'-'+mes+'-'+dia1+' '+hora+':'+minu+':'+seg
 latitud = '11.0193'
 longitud = '-74.8504'
 bd = mysql.connector.connect(user='root', password='1234', host='localhost', database='coordenadas')
