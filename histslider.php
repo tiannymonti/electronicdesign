511e37dde7f5bc5beef69221a6784eb09e1bc58b
tiannymonti <tiannymonti@gmail.com>
2016-09-20 15:39:27 -0500

    lenght

--- histslider.php
+++ histslider.php
@@ -281,7 +281,7 @@
 								contento = contento + tiempo + '\n';																		
 							}		
 							infowindow.setContent(contento);
-							infowindow.setPosition(event.latLng);
+							infowindow.setPosition(needle.latlng);
 							infowindow.open(map);										
 						}  //fin de la funcion de response					
 					}); //fin del ajax 						
