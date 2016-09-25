3997bbf632725c542f12c34c2ed6ef8c3c215840
tiannymonti <tiannymonti@gmail.com>
2016-09-25 12:25:38 -0500

    I can show you a 1000 ways to kill a fly

--- flykiller.php
+++ flykiller.php
@@ -108,9 +108,6 @@
    
     <script type="text/javascript" src="pickadate.js/lib/picker.time.js"></script>
     <script async defer src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDIz0DiW7sx_Ra06WAb9dSm-QURV-WTZGM"></script>
-    <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
         
     <script type="text/javascript">		 
 		 var respuesta;
