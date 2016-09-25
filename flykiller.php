76a0d77cdad04f6775f9769bbd895383bc130394
tiannymonti <tiannymonti@gmail.com>
2016-09-25 13:13:19 -0500

    I can show you a 1000 ways to kill a fly

--- flykiller.php
+++ flykiller.php
@@ -68,6 +68,8 @@
 		  <input type="range" style="margin: auto;" data-rangeslider>
 		  <span class="thumb active" style="margin: auto">
 		  <span class="value"></span>
+		 </div>
+		<div class="row">
 		  <div class="preloader-wrapper big active" style="display:none; margin:auto;" id="preloader">
 			<div class="spinner-layer spinner-blue-only">
 			  <div class="circle-clipper left">
@@ -113,22 +115,6 @@
     <script async defer src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDIz0DiW7sx_Ra06WAb9dSm-QURV-WTZGM"></script>
     <script src="jquery.min.js"></script>
 	<script type="text/javascript" src="rangeslider.js/rangeslider.min.js"></script>
-<!--
-	<script>
-		// Initialize a new plugin instance for all
-		// e.g. $('input[type="range"]') elements.
-		$('input[type="range"]').rangeslider();
-
-		//// Destroy all plugin instances created from the
-		//// e.g. $('input[type="range"]') elements.
-		//$('input[type="range"]').rangeslider('destroy');
-
-		//// Update all rangeslider instances for all
-		//// e.g. $('input[type="range"]') elements.
-		//// Usefull if you changed some attributes e.g. `min` or `max` etc.
-		//$('input[type="range"]').rangeslider('update', true);
-	</script>
--->
         
     <script type="text/javascript">		 
 		 var respuesta;
@@ -141,6 +127,24 @@
 		 var hora1;
 		 var hora2;
 		 var myPath;
+		 
+		 $('input[type="range"]').rangeslider({
+
+			// Feature detection the default is `true`.
+			// Set this to `false` if you want to use
+			// the polyfill also in Browsers which support
+			// the native <input type="range"> element.
+			polyfill: true,
+
+			// Default CSS classes
+			rangeClass: 'rangeslider',
+			disabledClass: 'rangeslider--disabled',
+			horizontalClass: 'rangeslider--horizontal',
+			verticalClass: 'rangeslider--vertical',
+			fillClass: 'rangeslider__fill',
+			handleClass: 'rangeslider__handle'
+			
+		});
 		 
 	
 		 var $input1 = $('.start-datepicker').pickadate({
