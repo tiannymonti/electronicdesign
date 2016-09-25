45de28b54e5c218aaea7a89b2f4fa6348fdd9483
tiannymonti <tiannymonti@gmail.com>
2016-09-25 12:44:59 -0500

    I can show you a 1000 ways to kill a fly

--- flykiller.php
+++ flykiller.php
@@ -115,14 +115,14 @@
 		// e.g. $('input[type="range"]') elements.
 		$('input[type="range"]').rangeslider();
 
-		// Destroy all plugin instances created from the
-		// e.g. $('input[type="range"]') elements.
-		$('input[type="range"]').rangeslider('destroy');
+		//// Destroy all plugin instances created from the
+		//// e.g. $('input[type="range"]') elements.
+		//$('input[type="range"]').rangeslider('destroy');
 
-		// Update all rangeslider instances for all
-		// e.g. $('input[type="range"]') elements.
-		// Usefull if you changed some attributes e.g. `min` or `max` etc.
-		$('input[type="range"]').rangeslider('update', true);
+		//// Update all rangeslider instances for all
+		//// e.g. $('input[type="range"]') elements.
+		//// Usefull if you changed some attributes e.g. `min` or `max` etc.
+		//$('input[type="range"]').rangeslider('update', true);
 	</script>
         
     <script type="text/javascript">		 
