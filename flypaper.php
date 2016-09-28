fcdc6537fbb990dc4fe3a9f8ba796e9b25aab2ac
tiannymonti <tiannymonti@gmail.com>
2016-09-27 23:37:09 -0500

    lol

--- flypaper.php
+++ flypaper.php
@@ -273,7 +273,7 @@
 			slider.noUiSlider.on('update', function( values, handle ) {
 				dateValues.innerHTML = myTimes[values[handle]];
 				marker.setPosition(myPositions[values[handle]]);
-				map.panTo(myPositions[values[handle]]);	
+				//map.panTo(myPositions[values[handle]]);	
 							
 			});	
 			 	 	 	 
