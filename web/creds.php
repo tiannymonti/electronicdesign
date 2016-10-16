93f2c16e79f0965be22eb701836c8a47c8d6c4ac
tiannymonti <tiannymonti@gmail.com>
2016-10-16 17:50:44 -0500

    l

--- web/creds.php
+++ web/creds.php
@@ -2,9 +2,9 @@
 
 // MySQL Credentials
 $db_host = 'localhost';
-$db_user = '';     // Enter your MySQL username
-$db_pass = '';     // Enter your MySQL password
-$db_name = 'torque';
+$db_user = 'root';     // Enter your MySQL username
+$db_pass = '1234';     // Enter your MySQL password
+$db_name = 'coordenadas';
 $db_table = 'raw_logs';
 
 // User credentials for Browser login
