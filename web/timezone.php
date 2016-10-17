9b58dd00e20be191c403110a8633a285556d86f5
tiannymonti <tiannymonti@gmail.com>
2016-10-16 19:06:35 -0500

    t

--- web/timezone.php
+++ web/timezone.php
@@ -1,5 +1,7 @@
 <?php
     session_set_cookie_params(0,dirname($_SERVER['SCRIPT_NAME']));
     session_start();
+    $time = $_GET['time'];
     $_SESSION['time'] = $_GET['time'];
+	echo time("H:i:s", $time);
 ?>
