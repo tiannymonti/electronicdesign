7bb7539ca91195605c554b3f6b1efc02f2014d90
tiannymonti <tiannymonti@gmail.com>
2016-10-16 20:56:24 -0500

    alicia adorada

--- js/init.js
+++ js/init.js
@@ -2,7 +2,15 @@
   $(function(){
 	  
 	
-	$(".dropdown-button").dropdown();
+	$(".dropdown-button").dropdown({
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: false, // Does not change width of dropdown to that of the activator
+		hover: true, // Activate on hover
+		gutter: 0, // Spacing from edge
+		belowOrigin: true, // Displays dropdown below the button
+		alignment: 'left' // Displays dropdown with edge aligned to the left of button		
+	});
         
     $('.button-collapse').sideNav({
 		edge: 'left', // Choose the horizontal origin
