380bfe0e75af03f84d5002c8dcac9311bc4cdd7d
tiannymonti <tiannymonti@gmail.com>
2016-10-24 02:54:20 -0500

    Arriesgando mi inocencia

--- histdoble.php
+++ histdoble.php
@@ -83,7 +83,8 @@
 		</div>
 		</div>	
 		<div class="row">
-		<div class="col s6" id="connect" style="display:none; margin:auto; width:50%"></div>
+		<div class="col s6 push-6" id="connect" style="display:none; margin:auto; width:50%"></div>
+		<div class="col s6 pull-6" style="display:none; margin:auto; width:50%">LOOOL</div>
 		</div>
 		<div class="row" style="margin:auto;">
 			<div class="col s6 push-s6" style="display:none; margin:auto; text-align: center;" id = "values"><span class="flow-text"></span></div>
