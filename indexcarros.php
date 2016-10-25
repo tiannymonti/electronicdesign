aa3f80744ef10ea4640d3898c46cc0e347ad1a16
tiannymonti <tiannymonti@gmail.com>
2016-10-24 21:47:29 -0500

    m

--- indexcarros.php
+++ indexcarros.php
@@ -121,6 +121,39 @@ var marker2;
 var myVal = consulta();
 var myPositions = [];
 var myPositions2 = [];
+
+	function CenterControl(controlDiv, map) {
+
+	  // Set CSS for the control border.
+	  var controlUI = document.createElement('div');
+	  controlUI.style.backgroundColor = '#fff';
+	  controlUI.style.border = '2px solid #fff';
+	  controlUI.style.borderRadius = '3px';
+	  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+	  controlUI.style.cursor = 'pointer';
+	  controlUI.style.marginBottom = '22px';
+	  controlUI.style.textAlign = 'center';
+	  controlUI.title = 'Click to recenter the map';
+	  controlDiv.appendChild(controlUI);
+
+	  // Set CSS for the control interior.
+	  var controlText = document.createElement('div');
+	  controlText.style.color = 'rgb(25,25,25)';
+	  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+	  controlText.style.fontSize = '16px';
+	  controlText.style.lineHeight = '38px';
+	  controlText.style.paddingLeft = '5px';
+	  controlText.style.paddingRight = '5px';
+	  controlText.innerHTML = 'Center Map';
+	  controlUI.appendChild(controlText);
+
+	  // Setup the click event listeners: simply set the map to Chicago.
+	  controlUI.addEventListener('click', function() {
+		map.setCenter(myCenter2);
+	  });
+
+	}
+
 	function consulta(){
 		
 		$.ajax({
@@ -241,8 +274,14 @@ google.maps.event.addListener(map, 'click', function(event) {
    map.setZoom(9);
    map.setCenter(marker.getPosition());
   });
- 
+  
+    // Create the DIV to hold the control and call the CenterControl() constructor
+  // passing in this DIV.
+  var centerControlDiv = document.createElement('div');
+  var centerControl = new CenterControl(centerControlDiv, map);
 
+  centerControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
