9bdeed76cc7f28919bacff87d416250048ca7135
rainerr19 <rainerr19@gmail.com>
2016-11-02 00:00:50 -0500

    solo rasp
    arregle detalles

--- sensor.py
+++ sensor.py
@@ -1,23 +1,23 @@
+
 ## sensor
 
 import RPi.GPIO as GPIO
 import time
 import serial
 
-syrus = serial.Serial('/dev/ttyUSB0',baudrate=115200)
+#syrus = serial.Serial('/dev/ttyUSB0',baudrate=115200)
 def medidas():
     GPIO.setmode(GPIO.BCM)
 
-    TRIG = 8
-    ECHO = 7
+    TRIG = 2
+    ECHO = 3
 
     print "Distance Measurement In Progress"
 
     GPIO.setup(TRIG,GPIO.OUT)
     GPIO.setup(ECHO,GPIO.IN)
-    itera = raw_input("numero de iteraciones: ")
 
-    
+
     GPIO.output(TRIG, False)
     print "Esperando distancia.."
     print " "
@@ -27,10 +27,10 @@ def medidas():
     GPIO.output(TRIG, False)
     while GPIO.input(ECHO)==0:
         pulse_start = time.time()
-        
+
     while GPIO.input(ECHO)==1:
         pulse_end = time.time()
-        
+
     pulse_duration = pulse_end - pulse_start
 
     distance = pulse_duration * 17150
@@ -41,10 +41,11 @@ def medidas():
     return distance
     GPIO.cleanup()
 
-while true:
-    m = medindas()
+while True:
+    m = medidas()
     #syrus.write(m)
     print(m)
     time.sleep(0.5)
+
 
 
